d226ffe0cb893df379a1697923c8cda5d1d0c289
ajujacob <ajujacob88@gmail.com>
2024-03-01 12:58:55 +0530

    using enums to create a cstom type category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,11 +2,14 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid
             .v4(); //this then generates a unique string id and assigns it as an initial value to the id property whenever this expense clas is initialised.. Uuid is a third part package installed using flutter pub add uuid
 
@@ -14,4 +17,7 @@ class Expense {
   final String title;
   final double amount;
   final DateTime date;
+  //final String category; //insted we need some fixed categories, so use enums
+  final Category category;
+  //so by using enums we are now using a custom type of category
 }
