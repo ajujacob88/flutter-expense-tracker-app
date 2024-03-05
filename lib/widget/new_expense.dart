7e68846ed6ad68e3fa05c568110899b8e4e82a3e
ajujacob <ajujacob88@gmail.com>
2024-03-05 10:14:52 +0530

    getting user input on every keystroke

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -10,17 +10,34 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
             // keyboardType: TextInputType.text,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(label: Text('Title')),
           ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_enteredTitle);
+                  //now do the actions for enteredTitle.. This is one way of doing...we can verify that the enteredtitle is saved using the print in debug console
+                },
+                child: const Text('Save Expense'),
+              )
+            ],
+          )
         ],
       ),
     );
