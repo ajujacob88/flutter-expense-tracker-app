26c19b6f86361098ed6916489e04a11802ff1667
ajujacob <ajujacob88@gmail.com>
2024-03-05 11:29:25 +0530

    adding amount textfield also

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -17,10 +17,13 @@ class _NewExpenseState extends State<NewExpense> {
 
   final _titleController = TextEditingController();
 
+  final _amountController = TextEditingController();
+
 //the controller should be disposed/deleted after its use,, so the below method is necessary while using textediting contoller
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -33,18 +36,31 @@ class _NewExpenseState extends State<NewExpense> {
           TextField(
             controller: _titleController,
             maxLength: 50,
-            // keyboardType: TextInputType.text,
+            // keyboardType: TextInputType.text, //this is default, so no need to specify
             decoration: const InputDecoration(label: Text('Title')),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '₹ ',
+              label: Text('Amount'),
+            ),
           ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
                   //now do the actions for enteredTitle.. This is one way of doing...we can verify that the enteredtitle is saved using the print in debug console
+                  print(_amountController.text);
                 },
                 child: const Text('Save Expense'),
-              )
+              ),
             ],
           )
         ],
