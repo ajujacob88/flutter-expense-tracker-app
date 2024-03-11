621496685a695794e22b127cfba42f8390115eab
ajujacob <ajujacob88@gmail.com>
2024-03-11 14:20:35 +0530

    locking the device orientation to portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widget/expenses.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
@@ -11,65 +12,71 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+  //widgetsflutterbinding and systemchrome sets the orientation and here i set the app will run only as on portrait mode..locking the device orientation to portrait mode
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           ),
-        ),
-      ),
-      //above is the dark theme and below is the light theme,, it can be controlled by the thememode parameter in the material app (given at the end)
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        //for chnaging the card theme
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          // margin: const EdgeInsets.all(16),
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-
-        // cardTheme: ThemeData().cardTheme.copyWith(
-        //       color: kColorScheme.secondaryContainer,
-        //       // margin: const EdgeInsets.all(16),
-        //       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        //     ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 16,
-              ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
             ),
+          ),
+        ),
+        //above is the dark theme and below is the light theme,, it can be controlled by the thememode parameter in the material app (given at the end)
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          //for chnaging the card theme
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            // margin: const EdgeInsets.all(16),
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
 
-        // textTheme: const TextTheme().copyWith(
-        //   titleLarge: TextStyle(
-        //     fontWeight: FontWeight.normal,
-        //     color: kColorScheme.onSecondaryContainer,
-        //     fontSize: 16,
-        //   ),
-        // ),
+          // cardTheme: ThemeData().cardTheme.copyWith(
+          //       color: kColorScheme.secondaryContainer,
+          //       // margin: const EdgeInsets.all(16),
+          //       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          //     ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 16,
+                ),
+              ),
+
+          // textTheme: const TextTheme().copyWith(
+          //   titleLarge: TextStyle(
+          //     fontWeight: FontWeight.normal,
+          //     color: kColorScheme.onSecondaryContainer,
+          //     fontSize: 16,
+          //   ),
+          // ),
+        ),
+        //themeMode: ThemeMode.light,
+        //themeMode: ThemeMode.system,
+        // above thememode.system is default, so no need to specify it again, it will automatically be chosed,,, Thememode.system means chose dark or light mode according to the user os setting saved by user,, Thememode.dark means always set the app to run on dark mode
+        home: const Expenses(),
       ),
-      //themeMode: ThemeMode.light,
-      //themeMode: ThemeMode.system,
-      // above thememode.system is default, so no need to specify it again, it will automatically be chosed,,, Thememode.system means chose dark or light mode according to the user os setting saved by user,, Thememode.dark means always set the app to run on dark mode
-      home: const Expenses(),
-    ),
-  );
+    );
+  });
 }
